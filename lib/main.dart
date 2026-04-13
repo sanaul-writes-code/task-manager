e6c17ffacf7e4249210d69ef8db79666c6a4681d
sanaul-writes-code <165845997+sanaul-writes-code@users.noreply.github.com>
2026-04-12 22:11:37 -0400

    setup Flutter project and initialize Firebase

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Task Manager',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Firebase Connected Successfully',
+            style: TextStyle(fontSize: 36),
+          ),
+        ),
+      ),
+    );
+  }
+}
